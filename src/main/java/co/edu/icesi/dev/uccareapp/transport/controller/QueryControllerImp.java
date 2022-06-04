fc8059d354053f12d16969fcececd8adc3034d5b
sebastianvilla17 <1193269834@u.icesi.edu.co>
2022-06-04 15:05:52 -0500

    firts query

--- src/main/java/co/edu/icesi/dev/uccareapp/transport/controller/QueryControllerImp.java
+++ src/main/java/co/edu/icesi/dev/uccareapp/transport/controller/QueryControllerImp.java
@@ -1,68 +1,54 @@
 package co.edu.icesi.dev.uccareapp.transport.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import co.edu.icesi.dev.uccareapp.transport.service.PurchaseorderheaderServiceImp;
 
 @Controller
 public class QueryControllerImp implements QueryController {
    
     @Autowired
-    BillofmaterialService billofmaterialservice;
+    PurchaseorderheaderServiceImp purchaseservice;
 
-    @Autowired
-    ProductService productservice;
-
-    @GetMapping("/billofmaterials")
+    @GetMapping("/queries")
     public String index(Model model) {
-        model.addAttribute("billofmaterials", billofmaterialservice.findAll());
-        return "billofmaterials/index";
+        return "queries/index";
     }
 
-    @GetMapping("/billofmaterials/add")
+    @GetMapping("/queries/queryrange")
     public String showSaveForm(Model model) {
-        model.addAttribute("billofmaterial", new Billofmaterial());
-        model.addAttribute("products", productservice.findAll() );
-        return "billofmaterials/add-billofmaterial";
+        model.addAttribute("date1", new  String());
+        model.addAttribute("date2", new String());
+        model.addAttribute("result", new Object());
+        return "queries/bydates";
     }
 
-    @PostMapping("/billofmaterials/add") 
-    public String saveBillofmaterial(@Validated @ModelAttribute Billofmaterial billofmaterial, BindingResult bindingResult, 
-            Model model, @RequestParam(value = "action", required = true) String action) {
-        if (!action.equals("Cancel")) {
+    @PostMapping("/queries/queryrange") 
+    public String saveBillofmaterial(@Validated @ModelAttribute String date1, @Validated @ModelAttribute String date2
+    ,BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+
             if (bindingResult.hasErrors()) {
-                return "billofmaterials/add-billofmaterial";
+                return "queries/bydates";
             }
-            
-            billofmaterialservice.saveBillofmaterial(billofmaterial);
-        }
-        return "redirect:/billofmaterials/";
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate ld1 = LocalDate.parse(date1, formatter);
+            LocalDate ld2 = LocalDate.parse(date2, formatter);
+             model.addAttribute("results", purchaseservice.findByDateRange(ld1, ld2));
+        
+        return "redirect:/queries/queriesrange";
     }
 
-    @GetMapping("/billofmaterials/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        Optional<Billofmaterial> billofmaterialop = billofmaterialservice.findById(id);
-        if (billofmaterialop == null)
-            throw new IllegalArgumentException("Invalid billofmaterial Id:" + id);
-        model.addAttribute("billofmaterial", billofmaterialop.get());
-        model.addAttribute("products", productservice.findAll());
-        return "billofmaterials/update-billofmaterial";
-    }
 
-    @PostMapping("/billofmaterials/edit/{id}")
-    public String updateBillofmaterial(@PathVariable("id") Integer id, @RequestParam(value = "action", required = true) String action, 
-            @Validated @ModelAttribute Billofmaterial billofmaterial, BindingResult bindingResult, Model model ) {
-        if (!action.equals("Cancel")) {
-            if (bindingResult.hasErrors()) {
-                return "billofmaterials/update-billofmaterial";
-            }
-            billofmaterialservice.editBillofmaterial(billofmaterial);
-        }
-        return "redirect:/billofmaterials/";
-    }
-
-    @GetMapping("/billofmaterials/del/{id}")
-    public String deleteShipmethod(@PathVariable("id") Integer id, Model model) {
-        Billofmaterial billofmaterial = billofmaterialservice.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid billofmaterial Id:" + id));
-        billofmaterialservice.delete(billofmaterial);
-        return "redirect:/billofmaterials/";
-    }
 }
